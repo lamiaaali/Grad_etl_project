d6f4a97f005521580d238fb8c65d2eabe74c7d47
Mahmood Sayed sherif <vavytarazan65@gmail.com>
2024-10-16 20:28:14 +0300

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,60 @@
+-- Create database 
+CREATE DATABASE Customer_tickets;
+
+-- use DB
+USE Customer_tickets;
+
+-- Create a star schema DWH 
+-- 1-Create dim tables
+CREATE TABLE Customer_Dim (
+    Customer_ID INT PRIMARY KEY ,
+    Customer_Name NVARCHAR(255),
+    Customer_Email NVARCHAR(255),
+    Customer_Age INT,
+    Customer_Gender NVARCHAR(255)
+);
+
+CREATE TABLE Product_Dim (
+    Product_ID INT PRIMARY KEY ,
+    Product_Purchased NVARCHAR(255)
+);
+
+
+CREATE TABLE Date_Dim (
+    Date_ID INT PRIMARY KEY ,
+    Date_of_Purchase DATE,
+    Year INT,
+    Month INT,
+    Day_of_Week NVARCHAR(255)
+);
+
+
+CREATE TABLE Ticket_Dim (
+    Ticket_ID INT PRIMARY KEY ,
+    Ticket_Type NVARCHAR(255),
+	Ticket_Subject	NVARCHAR(255),
+	Ticket_Status	NVARCHAR(255),
+	Ticket_Priority	NVARCHAR(255),
+	Ticket_Channel NVARCHAR(255)
+
+);
+
+
+-- CReate fact table
+CREATE TABLE Ticket_Fact (
+    Ticket_ID INT PRIMARY KEY,
+    Customer_ID INT,
+    Product_ID INT,
+    Date_ID INT,
+    Ticket_Type_ID INT,
+    Ticket_Status_ID INT,
+    Ticket_Priority_ID INT,
+    Ticket_Channel_ID INT,
+    First_Response_Time DATETIME,
+    Time_to_Resolution DATETIME,
+    Customer_Satisfaction_Rating INT,
+    FOREIGN KEY (Customer_ID) REFERENCES Customer_Dim(Customer_ID),
+    FOREIGN KEY (Product_ID) REFERENCES Product_Dim(Product_ID),
+    FOREIGN KEY (Date_ID) REFERENCES Date_Dim(Date_ID),
+    FOREIGN KEY (Ticket_ID) REFERENCES Ticket_Dim(Ticket_ID),
+);
